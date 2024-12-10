e9386096b2f7fc2a8ea72bdb4519f69bcdc93150
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-12-11 00:57:14 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -5,13 +5,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-
-python manage.py migrate
 
 
-if [[$CREATE_SUPERUSER ]];
-then
-   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi             
 
